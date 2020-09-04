c2830123155facbdf083033b3706d192d08223f9
Noah Green <viriditymoon@gmail.com>
2020-09-04 11:22:27 -0400

    Finished up oop1.py tasks

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class
+class Vehicle(self):
+    pass
+
+# GroundVehicle and FlightVehicle are both direct subclasses of 
+# the base class
+class GroundVehicle(Vehicle, self):
+    pass
+class FlightVehicle(Vehicle, self):
+    pass
+
+# Car and Motorcycle are both direct subclasses of GroundVehicle
+class Car(GroundVehicle, self):
+    pass
+class Motorcycle(GroundVehicle, self):
+    pass
+
+# Starship and Airplane are both direct subclasses of
+# FlightVehicle
+class Starship(FlightVehicle, self):
+    pass
+class Airplane(FlightVehicle, self):
+    pass
